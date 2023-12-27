9b67b09915a38101eb84c66508e8d5e798678ad8
Kish <kisha_simpson@yahoo.com>
2023-12-27 16:54:22 -0600

    layout start

---
+++ src/components/AppBar.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Box from '@mui/material/Box';
+
+const settings = ['Profile', 'Account', 'Logout'];
+
+function AppBar() {
+    const [anchorElUser, setAnchorElUser] = useState(null);
+
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    return (
+        <MuiAppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1, boxShadow: 'none' }}>
+            <Toolbar>
+                <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '100%' }}>
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                        <img src="src/assets/imgs/LSPD.webp" alt="logo" width="150px" height="150px" />
+                        <Typography variant="h6" noWrap component="div" sx={{ marginLeft: '16px' }}>
+                            Mobile Database Terminal
+                        </Typography>
+                    </Box>
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                        <Tooltip title="Open settings">
+                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                <Avatar alt="Guest" src="/static/images/avatar/2.jpg" />
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{ mt: '45px' }}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            {settings.map((setting) => (
+                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                    <Typography textAlign="center">{setting}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                </Box>
+            </Toolbar>
+        </MuiAppBar>
+    );
+}
+
+export default AppBar;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Drawer from '@mui/material/Drawer';
+import Toolbar from '@mui/material/Toolbar';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
+const Sidebar = () => (
+    <Drawer
+        variant="permanent"
+        sx={{
+            width: 220,
+            flexShrink: 0,
+            [`& .MuiDrawer-paper`]: { width: 220, boxSizing: 'border-box', marginTop: '100px' },
+        }}
+    >
+        <Toolbar />
+        <Box sx={{ overflow: 'auto' }}>
+            <List>
+                {['Dashboard', 'Incidents', 'Reports', 'Profiles', 'Properties', 'Employment', 'Charges'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+    </Drawer>
+);
+
+export default Sidebar;
